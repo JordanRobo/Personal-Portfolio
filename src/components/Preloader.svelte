24dd62264851c99eb85bb84e57041d50679334b6
JordanRobo <jordan@stateot.art>
2023-11-17 17:33:59 +1100

    Added in logic and implimentation of a preloader

---
+++ src/components/Preloader.svelte
@@ -0,0 +1,7 @@
+<script>
+   import { fade } from 'svelte/transition';
+</script>
+  
+<div in:fade={{ duration: 2400 }} out:fade={{ duration: 2400 }} class="preloader flex justify-center items-center h-screen">
+    Loading...
+</div> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import Preloader from '../components/Preloader.svelte';
+
+    let isLoading = true;
+
+    onMount(async () => {
+        // Perform any initial data fetching or setup here
+        // Once done, set isLoading to false
+        setTimeout(() => { isLoading = false; }, 4800);
+    });
+</script>
+  
+{#if isLoading}
+  <Preloader />
+{:else}
+  <div in:fade={{ duration: 6400 }}>
+    <!-- Your main page content goes here -->
+    <h1 class="flex justify-center items-center h-screen">Page Title</h1>
+    <!-- Other page-specific content -->
+  </div>
+{/if}
